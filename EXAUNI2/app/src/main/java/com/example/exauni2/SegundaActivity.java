9ce7bfe15ef692dcf1785fe0e44d7aef6c25a3a0
EduardoPena27 <78942558+EduardoPena27@users.noreply.github.com>
2021-10-21 12:30:46 -0500

    Update SegundaActivity.java

--- EXAUNI2/app/src/main/java/com/example/exauni2/SegundaActivity.java
+++ EXAUNI2/app/src/main/java/com/example/exauni2/SegundaActivity.java
@@ -6,9 +6,20 @@ import android.os.Bundle;
 
 public class SegundaActivity extends AppCompatActivity {
 
+    private TextView tvJugador1, tvJugador2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+        tvJugador1 = (TextView) findViewById(R.id.tvJugador1);
+        tvJugador2 = (TextView) findViewById(R.id.tvJugador2);
+
+        Bundle bundle = getIntent().getExtras();
+        String nombre1 = bundle.getString("jugador1");
+        tvJugador1.setText(nombre1);
+        String nombre2 = bundle.getString("jugador2");
+        tvJugador2.setText(nombre2);
+
     }
 }

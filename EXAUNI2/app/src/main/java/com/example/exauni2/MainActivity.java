b5476adec169689ef02461130d71b4b6a1f94be9
EduardoPena27 <78942558+EduardoPena27@users.noreply.github.com>
2021-10-21 12:23:57 -0500

    Update MainActivity.java

--- EXAUNI2/app/src/main/java/com/example/exauni2/MainActivity.java
+++ EXAUNI2/app/src/main/java/com/example/exauni2/MainActivity.java
@@ -14,21 +14,29 @@ public class MainActivity extends AppCompatActivity {
 
     private Spinner spFichas;
     private EditText etJugador1, etJugador2;
-    private Button btIniciarJugego;
+    private final static int NOMBRE_REQUEST_CODE = 1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etJugador1 = (EditText) findViewById(R.id.etJugador1);
+        etJugador2 = (EditText) findViewById(R.id.etJugador2);
+
+
         spFichas = (Spinner) findViewById(R.id.spFichas);
-        etJugador1 =(EditText) findViewById(R.id.etJugador1);
-        etJugador2 =(EditText) findViewById(R.id.etJugador2);
-        btIniciarJugego = (Button) findViewById(R.id.btIniciarJugego);
         String[] fichas={"6", "12", "18","24"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, fichas);
         spFichas.setAdapter(adapter);
+
     }
 
     public void lanzarActivity(View view){
-        
+        //LANZAR LA SEGUNDA ACTIVITY
+        Intent intent = new Intent(this, SegundaActivity.class);
+        intent.putExtra("jugador1",etJugador1.getText().toString());
+        intent.putExtra("jugador2",etJugador2.getText().toString());
+        startActivityForResult(intent, NOMBRE_REQUEST_CODE);
     }
 }

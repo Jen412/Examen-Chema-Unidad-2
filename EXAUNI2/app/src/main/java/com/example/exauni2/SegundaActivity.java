a41668b78f0c76e2ddbf24afdfe93aed2fa376b1
Jen412 <72884006+Jen412@users.noreply.github.com>
2021-10-21 12:56:25 -0500

    c

--- EXAUNI2/app/src/main/java/com/example/exauni2/SegundaActivity.java
+++ EXAUNI2/app/src/main/java/com/example/exauni2/SegundaActivity.java
@@ -3,6 +3,7 @@ package com.example.exauni2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SegundaActivity extends AppCompatActivity {
 

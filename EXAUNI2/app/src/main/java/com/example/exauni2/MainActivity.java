49d222ba6ddf77d7537859cc8f44996474b92dfc
Jen412 <72884006+Jen412@users.noreply.github.com>
2021-10-21 10:10:04 -0500

    Agregar cosas

--- EXAUNI2/app/src/main/java/com/example/exauni2/MainActivity.java
+++ EXAUNI2/app/src/main/java/com/example/exauni2/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.exauni2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Spinner spFichas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spFichas = (Spinner) findViewById(R.id.spFichas);
+        String[] fichas={"6", "12", "18","24"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, fichas);
+        spFichas.setAdapter(adapter);
     }
 }

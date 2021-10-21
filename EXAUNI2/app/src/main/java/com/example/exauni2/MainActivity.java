9c783f4440f7ef58dc91d4e212982bb534e55602
Jen412 <72884006+Jen412@users.noreply.github.com>
2021-10-21 10:26:29 -0500

    terminar diseño MainActivity

--- EXAUNI2/app/src/main/java/com/example/exauni2/MainActivity.java
+++ EXAUNI2/app/src/main/java/com/example/exauni2/MainActivity.java
@@ -5,16 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
     private Spinner spFichas;
+    private EditText etJugador1, etJugador2;
+    private Button btIniciarJugego;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         spFichas = (Spinner) findViewById(R.id.spFichas);
+        etJugador1 =(EditText) findViewById(R.id.etJugador1);
+        etJugador2 =(EditText) findViewById(R.id.etJugador2);
+        btIniciarJugego = (Button) findViewById(R.id.btIniciarJugego);
         String[] fichas={"6", "12", "18","24"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, fichas);
         spFichas.setAdapter(adapter);
